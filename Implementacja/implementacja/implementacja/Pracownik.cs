ea127f1e575a49675600bee233219e4defe095c9
Bartek-Szewczyk <ba20001000@gmail.com>
2020-11-05 15:15:28 +0100

    1,2,3

---
+++ Implementacja/implementacja/implementacja/Pracownik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic;
+
+namespace implementacja
+{
+    class Pracownik
+    {
+        public string Nazwisko { get; set; }
+
+        public DateTime _dz;
+
+        public DateTime DataZatrudnienia
+        {
+            get;
+            set;
+        }
+
+        private decimal _wyn;
+        public decimal Wynagrodzenie
+        {
+            get => _wyn;
+            set => _wyn = (value < 0) ? 0 : value;
+        }
+
+        public override string ToString() => $"({Nazwisko.Trim()}, {DataZatrudnienia:d MMM yyyy}, {Wynagrodzenie} PLN)";
+ 
+    }
+   
+}
--- Implementacja/implementacja/implementacja/Program.cs
+++ Implementacja/implementacja/implementacja/Program.cs
@@ -6,7 +6,11 @@ namespace implementacja
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var p1 = new Pracownik();
+            p1.Wynagrodzenie = 100;
+            p1.Nazwisko = " Szewczyk ";
+            p1.DataZatrudnienia = new DateTime(2021, 10, 12);
+            Console.WriteLine(p1);
         }
     }
 }
